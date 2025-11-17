459a87ed6c0c0b3e737c0db60c5ea795fb99b4f6
kennethmdahl <kennethmdahl@gmail.com>
2025-11-17 22:28:35 +0000

    ditto

--- Wingtips/NewVenue.sql
+++ Wingtips/NewVenue.sql
@@ -1,30 +1,3 @@
  drop procedure if exists [dbo].[NewVenue]
  GO
-  -- Creates a new Venue
-  CREATE PROCEDURE [dbo].[NewVenue]
-      @VenueId  INT,
-      @VenueName NVARCHAR(128),
-      @VenueType NVARCHAR(30) = 'multipurpose',
-      @PostalCode NVARCHAR(20) = '98052',
-      @CountryCode CHAR(3) = 'USA'
-  AS
-      IF @VenueId IS NULL
-      BEGIN
-          RAISERROR ('Error. @VenueId must be specified', 11, 1)
-          RETURN 1
-      END
-  
-      IF @VenueName IS NULL
-      BEGIN
-          RAISERROR ('Error. @VenueName must be specified', 11, 1)
-          RETURN 1
-      END
-  
-  
-      -- Insert Venue
-      INSERT INTO [dbo].Venues
-          ([VenueId],[VenueName],[VenueType],[AdminEmail],[CountryCode], [PostalCode])         
-      VALUES
-          (@VenueId, @VenueName,@VenueType,'admin@email.com',@CountryCode, @PostalCode)
-  
-  RETURN 0
+ 
